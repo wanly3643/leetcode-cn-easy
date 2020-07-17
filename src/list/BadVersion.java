117a077bb0dc90092b10e53aa4271bbb350e98c8
Lei Wang <wanly3643@gmail.com>
2020-07-17 09:23:15 +0800

    20200717 +2;

---
+++ src/list/BadVersion.java
@@ -0,0 +1,54 @@
+/**
+ * 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。
+ *
+ * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
+ *
+ * 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。
+ *
+ * 示例:
+ *
+ * 给定 n = 5，并且 version = 4 是第一个错误的版本。
+ *
+ * 调用 isBadVersion(3) -> false
+ * 调用 isBadVersion(5) -> true
+ * 调用 isBadVersion(4) -> true
+ *
+ * 所以，4 是第一个错误的版本。
+ */
+package list;
+
+public class BadVersion {
+    private static class VersionControl {
+        int count = 0;
+        int error = 1702766719;
+        boolean isBadVersion(int version) {
+            count = count + 1;
+            return version < error;
+         }
+    }
+
+    private static class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            int h = 1;
+            int t = n;
+            int i;
+            while (h <= t) {
+                i = h + (t - h) / 2;
+                if (super.isBadVersion(i)) {
+                    t = i - 1;
+                } else {
+                    h = i + 1;
+                }
+            }
+
+            return h;
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+
+        System.out.println(s.firstBadVersion(2126753390) == s.error);
+        System.out.println("count: " + s.count);
+    }
+}
